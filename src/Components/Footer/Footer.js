2c2382fb891a331f2431addffce5dd743a8a1fe0
Ashish Bajracharya <lsr.files1@gmail.com>
2020-07-30 12:13:42 +0545

    server aded

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import './footer.css'
 export default function Footer() {
+    const CURRENT_YEAR = (new Date()).getFullYear()
     return (
-        <div>
-            footer from fotter component
+        <div className = "footer">
+           <Link to = "/" className = "footer__logo">
+                <img alt = "nba logo" src = "/images/nba_logo.png" />
+           </Link>
+           <div className = " footer__copyright">
+               @NBA {CURRENT_YEAR} all rights reserved
+           </div>
         </div>
     )
 }
